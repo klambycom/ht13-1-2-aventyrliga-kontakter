89d44a24b63e7ec0a7c6f7c637dbe3b1d3569cee
Christian Nilsson <christian@klamby.com>
2013-12-02 21:15:35 +0100

    Contact EF

---
+++ AdventurousContacts/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdventurousContacts.Models;
+using AdventurousContacts.Models.DataModels;
+
+namespace AdventurousContacts.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactEntities _entities = new ContactEntities();
+
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            var model = _entities.Contacts.ToList();
+
+            return View();
+        }
+
+    }
+}
